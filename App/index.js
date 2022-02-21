161655391355d8fc86fb338dc1ab245fd866c9eb
Jubayer Juhan <davidjuhan23@gmail.com>
2022-02-21 23:20:41 +0600

    POrt edited

--- App/index.js
+++ App/index.js
@@ -31,6 +31,6 @@ app.use('/api/v1', expense)
 app.use(error)
 
 // start the server
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 4000');
 })
