3f4c9ca4fdf985785e065ae4fe9b2c33891f03ce
Jubayer Juhan <davidjuhan23@gmail.com>
2022-02-21 22:27:02 +0600

    port changed

--- App/index.js
+++ App/index.js
@@ -31,6 +31,6 @@ app.use('/api/v1', expense)
 app.use(error)
 
 // start the server
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('Server is running on port 4000');
 })
